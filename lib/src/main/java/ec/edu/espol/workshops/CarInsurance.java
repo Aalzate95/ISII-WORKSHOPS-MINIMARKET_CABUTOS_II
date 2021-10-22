ec91230422ed88d710403120a46852db6f93837e
Juan Diego Vallejo <juadival@espol.edu.ec>
2021-10-22 16:45:31 -0500

    Checkstyle changes

--- lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,6 +1,8 @@
 package ec.edu.espol.workshops;
 
+
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 /**
  *
@@ -8,27 +10,28 @@ import java.util.Scanner;
  */
 
 public class CarInsurance {
+			static Logger log = Logger.getLogger(CarInsurance.class.getName());
 	
 		    public static void verificarJovenSoltero(Customer cliente){
-		        if(cliente.getSex().toUpperCase().equals("M") && !cliente.isMarried() && (cliente.getAge()<25)){
-		            cliente.setBase(cliente.getBase()+1500);
+		        if (cliente.getSex().toUpperCase().equals("M") && !cliente.isMarried() && (cliente.getAge()< 25)){
+		            cliente.setBase(cliente.getBase()+ 1500);
 		        }
 		    }
 		    
 		    public static void verificarMujeroCasado(Customer cliente){
-		        if(cliente.getSex().toUpperCase().equals("F")|| cliente.isMarried()){
-		            cliente.setBase(cliente.getBase()-200);
+		        if (cliente.getSex().toUpperCase().equals("F")|| cliente.isMarried()){
+		            cliente.setBase(cliente.getBase()- 200);
 		        }
 		    }
 		    
 		    public static void verificarEdad(Customer cliente){
-		        if(cliente.getAge()>=45 && cliente.getAge()<65){
-		            cliente.setBase(cliente.getBase()-100);
+		        if (cliente.getAge()>= 45 && cliente.getAge()< 65){
+		            cliente.setBase(cliente.getBase()- 100);
 		        }
 		    }
 		    
 		    public static void validarPremium(Customer cliente){
-		        if(cliente.getAge()>80 || !cliente.hasDrivingLicense()){
+		        if (cliente.getAge()> 80 || !cliente.hasDrivingLicense()){
 		        	System.out.println("Cannot sell insurance to current customer.");
 		        	System.exit(-1);
 		        }
@@ -37,9 +40,9 @@ public class CarInsurance {
 		    
 	
 		public static void main(String[] args) {
-	        try (// TODO code application logic here
+	        try (
 			Scanner sc = new Scanner(System.in)) {
-				System.out.println("Car Insurance Premium");
+				System.out.println("Car Insurance Premium"); // NOPMD by juandi on 10/22/21, 10:07 AM
 				
 				System.out.println("Enter your age: ");
 				String edad = sc.nextLine();
@@ -49,10 +52,9 @@ public class CarInsurance {
 				String estadoMarital = sc.nextLine();
 				boolean casado = true;
 				
-				if((estadoMarital.toUpperCase()).equals("Y")){
+				if ((estadoMarital.toUpperCase()).equals("Y")){
 				    casado = true;
-				}
-				else if((estadoMarital.toUpperCase()).equals("N")){
+				}else if ((estadoMarital.toUpperCase()).equals("N")){
 					casado = false;
 				}
 				
@@ -61,14 +63,13 @@ public class CarInsurance {
 				String licencia = sc.nextLine();
 				boolean tieneLicencia = true;
 				
-				if(licencia.toUpperCase().equals("Y")){
+				if (licencia.toUpperCase().equals("Y")){
 					tieneLicencia = true;
-				}
-				else if(licencia.toUpperCase().equals("N")){
+				}else if (licencia.toUpperCase().equals("N")){
 					tieneLicencia = false;
 				}
 				
-				Customer cliente = new Customer(Integer.parseInt(edad),sexo,casado,tieneLicencia);
+				Customer cliente = new Customer(Integer.parseInt(edad), sexo, casado, tieneLicencia);
 				
 				
 				verificarJovenSoltero(cliente);
@@ -77,9 +78,8 @@ public class CarInsurance {
 				
 				validarPremium(cliente);
 				
-				System.out.println("The premium's value to pay is $ " + cliente.getBase() + ".");
+				log.fine("The premium's value to pay is $ " + cliente.getBase() + ".");
 			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	        
